63634f2cb19a06dc724ff3024afeb2a8319f60e1
Oleksii Ozyrskyi <aozyrskyii@gmail.com>
2025-05-22 20:34:02 +0200

    dist dir

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
+    root: ".", 
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        about: "about-us.html",
+        blog: "blog.html",
+        contact: "contact-us.html",
+        blogItem: "blog-item.html", 
+      },
+    },
+  },
   plugins: [tailwindcss()],
 });
